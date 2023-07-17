2d7ffedbda35254a8ab65748b32e38d763e4fb4a
Joey Parrish <joeyparrish@google.com>
2023-07-17 14:00:25 -0700

    Fail with visible source changes at the failure point

--- packager/media/codecs/h264_parser_unittest.cc
+++ packager/media/codecs/h264_parser_unittest.cc
@@ -46,7 +46,7 @@ const uint8_t kPps2[] = {
 
 TEST(H264ParserTest, StreamFileParsing) {
   std::vector<uint8_t> buffer = ReadTestDataFile("test-25fps.h264");
-  ASSERT_FALSE(buffer.empty());
+  ASSERT_FALSE(buffer.empty() || !buffer.empty());
 
   // Number of NALUs in the test stream to be parsed.
   int num_nalus = 759;
